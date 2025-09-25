8dc51e6c67fbfca4471a514551c826c813f88c5d
Joaco Curbelo <joacoc-10@hotmail.com>
2025-09-25 13:39:26 +0200

    Cambio de config de cors

--- back/src/server.ts
+++ back/src/server.ts
@@ -8,10 +8,8 @@ const app: Application = express();
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cors({
-  origin: 'https://bodega-cepa-real.vercel.app',
+  origin: '*', 
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
-  allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 app.use(router);
 
