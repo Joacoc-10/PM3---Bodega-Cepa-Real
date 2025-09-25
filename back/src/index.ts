b81f96fea23628e7823da230af5b9853baeb30be
Joaco Curbelo <joacoc-10@hotmail.com>
2025-09-25 20:41:11 +0200

    Cambios de index

--- back/src/index.ts
+++ back/src/index.ts
@@ -2,15 +2,16 @@ import app from "./server";
 import "reflect-metadata";
 import { AppDataSource } from "./config/data-source";
 
-const PORT = process.env.PORT;
+const PORT: number = process.env.PORT ? parseInt(process.env.PORT, 10) : 8080;
 
 AppDataSource.initialize()
   .then(() => {
-    app.listen(PORT, () => {
+    app.listen(PORT, "0.0.0.0", () => {
       console.log(`El servidor esta escuchando en el puerto ${PORT}`);
     });
   })
   .catch((error) => {
-    console.log(`Error al conectar la base de datos`);
+    console.log("Error al conectar la base de datos");
     console.log(error);
   });
+
